0bceed9f930082cd85eee41204894a08230c8714
Caio Silva <caiocoisa@gmail.com>
2021-04-20 00:41:37 -0300

    Incluindo tratamento de SQLException

--- src/main/java/aula1/persistence/ProfessorDao.java
+++ src/main/java/aula1/persistence/ProfessorDao.java
@@ -27,62 +27,81 @@ public class ProfessorDao implements Dao<Professor>{
 	
 	@Override
 	public void createTable() throws SQLException {
-		Statement statement = this.conexao.createStatement();
-		statement.execute("CREATE TABLE professores"
-				+ "(id BIGSERIAL,"
-				+ "nome VARCHAR(255),"
-				+ "idade BIGINT,"
-				+ "PRIMARY KEY (id) );");
+		try {
+			Statement statement = this.conexao.createStatement();
+			statement.execute("CREATE TABLE professores"
+					+ "(id BIGSERIAL,"
+					+ "nome VARCHAR(255),"
+					+ "idade BIGINT,"
+					+ "PRIMARY KEY (id) );");
+		}catch (PSQLException e) {
+			System.err.println("Ocorreu um erro ao criar a tabela.");
+			System.err.println(e.getMessage());
+		}
 	}
 
 	@Override
 	public void insert(Professor professor) throws SQLException {
-		PreparedStatement preparedStatement = this.conexao.prepareStatement( ""
-				+ "INSERT INTO professores (nome, idade) "
-				+ "VALUES (?, ?);");
-		preparedStatement.setString(1, professor.getNome());
-		preparedStatement.setLong(2, professor.getIdade());
-		preparedStatement.execute();
+		try {
+			PreparedStatement preparedStatement = this.conexao.prepareStatement( ""
+					+ "INSERT INTO professores (nome, idade) "
+					+ "VALUES (?, ?);");
+			preparedStatement.setString(1, professor.getNome());
+			preparedStatement.setLong(2, professor.getIdade());
+			preparedStatement.execute();
+		}catch (PSQLException e) {
+			System.err.println("Ocorreu um erro ao inserir.");
+			System.err.println(e.getMessage());
+		}
 	}
 
 	@Override
 	public Professor findById(Long id) throws SQLException {
 		Professor professor = null;
-		
-		PreparedStatement preparedStatement = this.conexao.prepareStatement(""
-				+ "SELECT * FROM professores "
-				+ "WHERE id = ?;");
-		preparedStatement.setLong(1, id);
-		
-		ResultSet resultSet = preparedStatement.executeQuery();
-		
-		while ( resultSet.next() ) {
-			Long idProfessor = resultSet.getLong("id");
-			String nome = resultSet.getString( "nome" );
-			Integer idade = resultSet.getInt( "idade" );
+		try {
+			PreparedStatement preparedStatement = this.conexao.prepareStatement(""
+					+ "SELECT * FROM professores "
+					+ "WHERE id = ?;");
+			preparedStatement.setLong(1, id);
 			
-			professor = new Professor(idProfessor, nome, idade);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			while ( resultSet.next() ) {
+				Long idProfessor = resultSet.getLong("id");
+				String nome = resultSet.getString( "nome" );
+				Integer idade = resultSet.getInt( "idade" );
+				
+				professor = new Professor(idProfessor, nome, idade);
+			}
+		}catch (PSQLException e) {
+			System.err.println("Ocorreu um erro ao consultar.");
+			System.err.println(e.getMessage());
 		}
+		
 		return professor;
 	}
 
 	@Override
 	public List<Professor> findAll() throws SQLException {
 		ArrayList<Professor> professores = new ArrayList<>();
-		
-		PreparedStatement  preparedStatement = this.conexao.prepareStatement(""
-				+ "SELECT * FROM professores;");
-		
-		ResultSet resultSet = preparedStatement.executeQuery();
-		
-		while ( resultSet.next() ) {
-			Long id = resultSet.getLong("id");
-			String nome = resultSet.getString("nome");
-			Integer idade = resultSet.getInt("idade");
+		try {
+			PreparedStatement  preparedStatement = this.conexao.prepareStatement(""
+					+ "SELECT * FROM professores;");
 			
-			Professor professor = new Professor(id, nome, idade);
-			professores.add(professor);
-		}
+			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			while ( resultSet.next() ) {
+				Long id = resultSet.getLong("id");
+				String nome = resultSet.getString("nome");
+				Integer idade = resultSet.getInt("idade");
+				
+				Professor professor = new Professor(id, nome, idade);
+				professores.add(professor);
+			}
+		}catch (PSQLException e) {
+			System.err.println("Ocorreu um erro ao consultar.");
+			System.err.println(e.getMessage());
+		}			
 		return professores;
 	}
 
@@ -103,9 +122,7 @@ public class ProfessorDao implements Dao<Professor>{
 		
 		} catch (PSQLException e) {
 			System.err.println("Um erro ocorreu ao atualizar");
-			System.err.println(e.getSQLState());
 			System.err.println(e.getMessage());
-			System.err.println(e.getLocalizedMessage());
 		}
 		
 	}
@@ -113,13 +130,17 @@ public class ProfessorDao implements Dao<Professor>{
 	@Override
 	public void delete(Long id) throws SQLException {
 		Professor professor = findById(id);
-		
-		if ( !professor.equals(null) ) {		
-			PreparedStatement preparedStatement = this.conexao.prepareStatement(
-					"DELETE FROM professores WHERE id = ? ;");
-			preparedStatement.setLong(1, professor.getId());
-			preparedStatement.execute();
-		}
+		try {
+			if ( !professor.equals(null) ) {		
+				PreparedStatement preparedStatement = this.conexao.prepareStatement(
+						"DELETE FROM professores WHERE id = ? ;");
+				preparedStatement.setLong(1, professor.getId());
+				preparedStatement.execute();
+			}
+		}catch (PSQLException e) {
+			System.err.println("Ocorreu um erro ao excluir.");
+			System.err.println(e.getMessage());
+		}			
 	}
 
 }
